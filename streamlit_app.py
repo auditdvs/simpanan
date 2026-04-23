ed6085d0b23d667b023d2a246cb6ac39cf1fb2f6
auditdvs <dvsaudit@gmail.com>
2026-04-23 11:45:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,8 @@ def rolling_zscore(df: pd.DataFrame, window: int = 3, threshold: float = 1.0) ->
         g['Anomaly_HariRaya'] = (np.abs(g['Z_Score']) > threshold).astype(int)
         return g
 
-        result = weekly.groupby('ID', group_keys=False).apply(calc).reset_index(drop=True)
-        return result
+    result = weekly.groupby('ID', group_keys=False).apply(calc).reset_index(drop=True)
+    return result
 
 
 def aggregate_sukarela(df: pd.DataFrame) -> pd.DataFrame:
